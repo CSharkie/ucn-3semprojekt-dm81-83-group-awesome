735223d111c068f0fffd9f1c11dcec5d00dcda5b
muresan.gabriel.m@gmail.com <muresan.gabriel.m@gmail.com@ed1486cd-2464-ec37-b3bd-ee833fa52d63>
2013-11-29 20:39:32 +0000

    implemented some methods -- we need to change the database DPercent to decimal

--- trunk/Awesome/AwesomeServer/IServer.cs
+++ trunk/Awesome/AwesomeServer/IServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +10,21 @@ namespace AwesomeServer
     interface IServer
     {
         string createReservation(string name, bool taken, DateTime dateReserved, int movieId, int seatCount);
-        string createMovie(string title, DateTime dateAndTime,int roomId);
+        string createMovie(string title, DateTime dateAndTime, int roomId);
         string createRoom(int cols, int rows);
         string createTicket(decimal standard, int reservationId, int discountId);
+        string createDiscount(decimal dPercent);
         string updateReservation(int reservationId, string name, bool taken, int movieId, int seatCount);
         //string updateMovie(int movieId, string title, DateTime dateAndTime, int roomId);
         //string updateRoom(int roomId, int cols, int rows);
         //string updateTicket(int ticketId, decimal standard, int reservationId, int discountId);
         //string updateSeat(int seatId, int col, int row, bool usable, int roomId, int reservationId);
-        Reservation getReservation(int reservationId, string name, int movieId);
+        IList<Reservation> getReservation(int reservationId, string name, int movieId);
         //ICollection<Movie> getMovie(int movieId, string title, int roomId);
         Room getRoom(int roomId);
-        //ICollection<Ticket> getTicket(int ticketId, int reservationId);
-        //ICollection<Seat> getSeat(int seatId, int roomId);
+        //IList<Room> getAllRooms();
+        IList<Ticket> getTicket(int ticketId, int reservationId);
+        IList<Seat> getSeat(int seatId, int roomId, int col, int row);
         //string removeReservation(int reservationId, string name);
         //string removeMovie(int movieId, string title, DateTime dateAndTime, int roomId);
         //string removeRoom(int roomId);
